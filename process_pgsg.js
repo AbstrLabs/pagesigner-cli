34787a12e55ebf7c404318db38374d90ceeb615d
Bo Yao <icerove@gmail.com>
2021-12-19 00:40:38 +0800

    figure out iv to fit rfc-5288 and mina

--- process_pgsg.js
+++ process_pgsg.js
@@ -6,10 +6,10 @@ function buf2ocaml(buf) {
     for(let e of buf) {
         ret+='0x'
         let n = e.toString(16)
-        if (e.length < 2) {
-            e = '0'+e
+        if (n.length < 2) {
+            n = '0'+n
         }
-        ret+=e
+        ret+=n
         ret+='; '
     }
     ret+='|]'
@@ -25,9 +25,21 @@ async function main() {
     swkc = Buffer.from(swkc, 'base64')
     swkn = Buffer.from(swkn, 'base64')
     let swk = swkc.map((b, i) => b ^ swkn[i]);
+
+    let swic = pgsg['client server_write_iv share']
+    let swin = pgsg['notary server_write_iv share']
+    swic = Buffer.from(swic, 'base64')
+    swin = Buffer.from(swin, 'base64')
+    let swi = swic.map((b, i) => b ^ swin[i]);
+    let records = pgsg['server response records']
+
     console.log(swk)
     console.log(buf2ocaml(swk))
     console.log(swk.toString('hex'))
+    let rec = Buffer.from(records[0], 'base64').slice(0, 8)
+    let zero = Buffer.from([0,0,0,0])
+    let aesgcm_iv = Buffer.concat([swi, rec, zero])
+    console.log(buf2ocaml(aesgcm_iv))
 }
 
 main()
